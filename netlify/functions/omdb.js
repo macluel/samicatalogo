3b8f4891ba3a140a44c4a6352652d03b56657e5a
macluel <luelmacedo@gmail.com>
2026-06-20 21:38:38 -0300

    Update omdb.js

--- netlify/functions/omdb.js
+++ netlify/functions/omdb.js
@@ -1,9 +1,38 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "VERSION_12345",
-      env: process.env.OMDB_API_KEY || null
-    })
-  };
+const OMDB_BASE = 'https://www.omdbapi.com/';
+
+exports.handler = async (event) => {
+  try {
+    const apiKey = process.env.OMDB_API_KEY;
+
+    const params = event.queryStringParameters || {};
+    const url = new URL(OMDB_BASE);
+
+    Object.entries(params).forEach(([key, value]) => {
+      if (value) {
+        url.searchParams.set(key, value);
+      }
+    });
+
+    url.searchParams.set('apikey', apiKey);
+
+    const response = await fetch(url.toString());
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        Response: 'False',
+        Error: err.message
+      })
+    };
+  }
 };
